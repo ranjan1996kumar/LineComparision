a51f61a7ff995d1667240f3bfce11f012005eeb9
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-08-13 23:07:11 +0530

    calculating lenght

---
+++ LineComparison.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class LineComparison {
+    public static void main(String[] args) {
+        int x1 = 2;
+        int y1 = 5;
+        int x2 = 4;
+        int y2 = 8;
+        double result = Math.sqrt((x2 - x1) + (y2 - y1));
+        {
+            System.out.println("length of a line" + '\n' + result);
+        }
+    }
+}
